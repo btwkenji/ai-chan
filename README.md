39ff971aa61fd29eb3d31002d7359c8fe723ee1d
kenjitheman <kenjitheman@proton.me>
2023-08-02 21:39:01 +0300

    init

---
+++ README.md
@@ -0,0 +1,61 @@
+<h2 align="left">ai chan - telegram bot, openai api, for self use</h2>
+
+###
+
+<img align="right" height="250" src="https://media.tenor.com/hhXY7zorOG4AAAAC/hoshino-ai.gif"  />
+
+###
+
+<div align="left">
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" height="200" alt="javascript logo"  />
+  <img width="" />
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" height="200" alt="docker logo"  />
+</div>
+
+###
+
+<div align="center">
+  <a href="https://t.me/aichandaisukibest_bot" target="_blank">
+    <img src="https://img.shields.io/static/v1?message=ai chan&logo=telegram&label=&color=2CA5E0&logoColor=white&labelColor=&style=for-the-badge" height="35" alt="telegram logo"  />
+  </a>
+</div>
+
+###
+
+telegram-chat-bot
+chat bot, which is able to recognize your speech through voice messages and text through text messages
+
+## installation
+- use git clone to clone the git repository ->
+```
+git clone https://github.com/amodotomi/ai-chan
+```
+```
+npm install
+```
+this command will download and install all the dependencies defined in project
+
+## usage
+- inside config folder create production.json for production mode and default.json for development mode and inside paste your api keys like this ->
+```
+{
+    "TELEGRAM_TOKEN": "YOUR_TELEGRAM_TOKEN",
+    "OPENAI_API_KEY": "YOUR_OPENAI_API_KEY"
+}
+```
+### and then run dockerfile ->
+```
+docker build -t your_image_name .
+docker run -d -p 8080:80 your_image_name
+```
+
+## contributing
+
+- Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+- Please make sure to update tests as appropriate.
+
+## License
+
+- [MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/main.js
@@ -0,0 +1,82 @@
+import { session, Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import { code } from "telegraf/format";
+import config from "config";
+import { ogg } from "./ogg.js";
+import { openai } from "./openai.js";
+
+const INITIAL_SESSION = {
+  messages: [],
+};
+
+const bot = new Telegraf(config.get("TELEGRAM_TOKEN"));
+
+bot.use(session());
+
+bot.command("new", async (ctx) => {
+  ctx.session = INITIAL_SESSION;
+  await ctx.reply("waiting for new voice or text message ^_^");
+});
+
+bot.command("start", async (ctx) => {
+  ctx.session = INITIAL_SESSION;
+  await ctx.reply("waiting for new voice or text message ^_^");
+});
+
+bot.on(message("voice"), async (ctx) => {
+  ctx.session ??= INITIAL_SESSION;
+  try {
+    await ctx.reply(code("accepted, processing request..."));
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+    const userId = String(ctx.message.from.id);
+    const oggPath = await ogg.create(link.href, userId);
+    const mp3Path = await ogg.toMp3(oggPath, userId);
+
+    const text = await openai.transcription(mp3Path);
+    await ctx.reply(code(`response: ${text}`));
+
+    ctx.session.messages.push({
+      role: openai.roles.USER,
+      content: text,
+    });
+
+    const response = await openai.chat(ctx.session.messages);
+
+    ctx.session.messages.push({
+      role: openai.roles.ASSISTANT,
+      content: response.content,
+    });
+
+    await ctx.reply(response.content);
+  } catch (e) {
+    console.log(`error while voice message | `, e.message);
+  }
+});
+
+bot.on(message("text"), async (ctx) => {
+  ctx.session ??= INITIAL_SESSION;
+  try {
+    await ctx.reply(code("accepted, processing request..."));
+
+    ctx.session.messages.push({
+      role: openai.roles.USER,
+      content: ctx.message.text,
+    });
+
+    const response = await openai.chat(ctx.session.messages);
+
+    ctx.session.messages.push({
+      role: openai.roles.ASSISTANT,
+      content: response.content,
+    });
+
+    await ctx.reply(response.content);
+  } catch (e) {
+    console.log(`error while text message | `, e.message);
+  }
+});
+
+bot.launch();
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
